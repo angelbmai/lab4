67b11f2a9cb4bc0678433e53379e2f42a3a74da8
Angel Mai <abmai@ucsd.edu>
2021-01-28 10:56:52 -0800

    node.js lab

--- routes/hello.js
+++ routes/hello.js
@@ -4,7 +4,18 @@
  */
 
 exports.view = function(req, res){
-  res.render('index', {
-  	'name': '',
+	var nameToShow = req.params.userName;
+	console.log("my name is " + nameToShow);
+  res.render('hello', {
+  	'name': nameToShow,
   });
+
+  res.render("index",{
+	"name": nameToShow
+	/*"userName": "srk",
+	"firstName": "Klemmer",
+	"officeHours": "Tuesday",
+	"classes": ["Cogs120", "CSE170", "DSGN119"]*/
+  });
+
 };
